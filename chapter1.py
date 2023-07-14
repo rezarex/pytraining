5e0579bece886283e454fd67e4ab1961255a2ac5
rezarex <marvin.wekesa@gmail.com>
2023-07-14 21:37:57 +0300

    random sytuff

---
+++ chapter1.py
@@ -0,0 +1,13 @@
+# print("tri" +"vial"*3)
+'''
+TODO:
+research on:
+    textseq
+    string-methods
+    f-strings
+    formatstrings
+    old-string-formatting
+'''
+word = "python"
+#word[3]="x"
+print(word.rpartition("l"))
