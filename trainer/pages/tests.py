ba87a4065133097f78c949a1beda6d8418b0d447
rezarex <marvin.wekesa@gmail.com>
2023-07-23 23:41:56 +0300

    whatever this was

--- trainer/pages/tests.py
+++ trainer/pages/tests.py
@@ -1,3 +1,39 @@
 from django.test import TestCase
+from django.test import SimpleTestCase
+from django.urls import reverse
 
-# Create your tests here.
+class HomePageTest(SimpleTestCase):
+    def test_url_exists_at_location(self):
+        response = self.client.get("/")
+        self.assertEqual(response.status_code, 200)
+    
+    def test_home_url_name_available(self):
+        response = self.client.get(reverse("home"))
+        self.assertEqual(response.status_code,200)
+
+    def test_home_template_used(self):
+        response = self.client.get(reverse("home"))
+        self.assertTemplateUsed(response, "home.html")
+    
+    def test_home_template_contains(self):
+        response = self.client.get(reverse("home"))
+        self.assertContains(response, "<h1>Homepage</h1>")
+
+
+
+class AboutPageTest(SimpleTestCase):
+    def test_url_exists_at_location(self):
+        response = self.client.get("/about/")
+        self.assertEqual(response.status_code, 200)
+
+    def test_about_url_name_available(self):
+        response = self.client.get(reverse("about"))
+        self.assertEqual(response.status_code,200)
+
+    def test_about_template_used(self):
+        response = self.client.get(reverse("about"))
+        self.assertTemplateUsed(response, "about.html")
+
+    def test_about_template_contains(self):
+        response = self.client.get(reverse("about"))
+        self.assertContains(response, "<h1>About Page</h1>")
--- trainer/pages/views.py
+++ trainer/pages/views.py
@@ -1,9 +1,26 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+'''
+Generic Class based view implementation
+'''
+from django.views.generic import TemplateView
 
-'''
-back to basics
--->page 37
-'''
-def homeVIew(request):
-    return HttpResponse("Hello there")
+
+class HomePageView(TemplateView):
+    template_name = "home.html"
+
+class AboutView(TemplateView):
+    template_name = "about.html"
+
+
+
+
+
+
+# from django.shortcuts import render
+# from django.http import HttpResponse
+
+# '''
+# back to basics
+# -->page
+# '''
+# def homeVIew(request):
+#     return HttpResponse("Hello there")
